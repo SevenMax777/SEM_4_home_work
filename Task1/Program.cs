e070a872077c9be719504a14405b2221fb46f650
Max <seven.85@list.ru>
2024-02-29 15:21:36 +0500

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Введите целое число (q - для выхода): ");
+            string input = Console.ReadLine()!;
+
+            if (input == "q")
+            {
+                break;
+            }
+
+            int number;
+            if (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Неверный формат числа.");
+                continue;
+            }
+
+            int sumOfDigits = 0;
+            while (number > 0)
+            {
+                sumOfDigits += number % 10;
+                number /= 10;
+            }
+
+            if (sumOfDigits % 2 == 0)
+            {
+                Console.WriteLine("Сумма цифр числа чётная. Завершение работы.");
+                break;
+            }
+        }
+    }
+}
