5be4441dca781b9cb5127f23cb60215ba6d10c43
Max <seven.85@list.ru>
2024-02-29 15:46:46 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 2, 3, 4, 5 };
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+	int temp = array[i];
+	array[i] = array[array.Length - 1 - i];
+	array[array.Length - 1 - i] = temp;
+}
+
+foreach (int element in array)
+{
+   Console.Write(element + " ");
+}
